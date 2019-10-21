95a8ab41fd3959a3b366f046084aa1b63d24fcba
Garfdev <Garfdev.13@gmail.com>
2019-10-21 15:37:54 +0700

    Updated AI to search function

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ function App() {
     }
 
   const getTrailer = async (id) => {
-      const response  = await fetch(`http://api.themoviedb.org/3/movie/${id}/videos?api_key=a3bab0e1a8803be8f21fc84e04666349`)
+      const response  = await fetch(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=a3bab0e1a8803be8f21fc84e04666349`)
       const data = await response.json()
       setModalTrailerId(data.results[0].key)
   }
